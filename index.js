d65d897573dda78ebc3c9b83f8e89b78091434ed
ZeDoExter <navapoltawan@gmail.com>
2023-01-09 18:15:18 +0700

    fix all

--- index.js
+++ index.js
@@ -1,21 +1,20 @@
-require('dotenv').config()
-const express = require('express')
-const app = express()
-
-const dayjs = require('dayjs')
-const utc = require('dayjs/plugin/utc')
-const timezone = require('dayjs/plugin/timezone')
+require('dotenv').config();
+const express = require('express');
+const hbs = require('hbs');
+const dayjs = require('dayjs');
+const utc = require('dayjs/plugin/utc');
+const timezone = require('dayjs/plugin/timezone');
+require('./db');
+const generalRouter = require('./routers/general');
+const postsRouter = require('./routers/posts');
 
 dayjs.extend(utc);
-dayjs.extend(timezone)
-dayjs.tz.setDefault('Asia/Bangkok')
+dayjs.extend(timezone);
+dayjs.tz.setDefault('Asia/Bangkok');
 
 const port = process.env.PORT || 3000;
 
 const hbs = require('hbs')
-
-const generalRouter = require('./routers/general')
-const postsRouter = require('./routers/posts')
 
 app.use(express.urlencoded({ extended: true}))
 app.set('view engine', 'hbs')
